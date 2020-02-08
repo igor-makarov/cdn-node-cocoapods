46af86432d0a018dc78d214f0c3d6ce958621333
Igor Makarov <igormaka@gmail.com>
2020-02-08 16:39:16 +0200

    re-request HTTP2 error

--- request.js
+++ request.js
@@ -3,4 +3,18 @@ let {request} = require('http2-wrapper')
 
 module.exports = {}
 module.exports.http = got
-module.exports.http2 = got.extend({request})
+module.exports.http2 = got.extend({
+  retry: {
+    errorCodes: [
+      'ERR_HTTP2_STREAM_ERROR',
+      'ETIMEDOUT',
+      'ECONNRESET',
+      'EADDRINUSE',
+      'ECONNREFUSED',
+      'EPIPE',
+      'ENOTFOUND',
+      'ENETUNREACH',
+      'EAI_AGAIN'
+    ]
+  }
+}, {request})
