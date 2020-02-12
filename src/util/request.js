db9e3f6e5fbbbe763b84e65e570b435d3a98e199
Igor Makarov <igormaka@gmail.com>
2020-02-12 15:24:11 +0200

    remove unused packages

--- src/util/request.js
+++ src/util/request.js
@@ -1,23 +1,6 @@
 let got = require('got')
-let {request} = require('http2-wrapper')
 
 module.exports = {}
 module.exports.http = got.extend({
   throwHttpErrors: false
 })
-module.exports.http2 = got.extend({
-  throwHttpErrors: false,
-  retry: {
-    errorCodes: [
-      'ERR_HTTP2_STREAM_ERROR',
-      'ETIMEDOUT',
-      'ECONNRESET',
-      'EADDRINUSE',
-      'ECONNREFUSED',
-      'EPIPE',
-      'ENOTFOUND',
-      'ENETUNREACH',
-      'EAI_AGAIN'
-    ]
-  }
-}, {request})
